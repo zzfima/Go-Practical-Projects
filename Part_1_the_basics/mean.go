4e8b17c53c71a68a8d5163d7baf79c4203eff9f0
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-11-05 08:30:31 +0200

    mean

--- Part_1_the_basics/mean.go
+++ Part_1_the_basics/mean.go
@@ -6,4 +6,15 @@ func main() {
 	fmt.Println("Calculate mean of next numbers:")
 	numbers := []int{4, 5, 6}
 	fmt.Println(numbers)
+
+	s := sum(numbers)
+	fmt.Println("Mean is ", s/len(numbers))
+}
+
+func sum(numbers []int) int {
+	s := 0
+	for _, value := range numbers {
+		s += value
+	}
+	return s
 }
